8475a93edacaeea667d29b03857227a2e5455e1e
Noel M Nguemechieu <48719845+nguemechieu@users.noreply.github.com>
2022-04-02 18:04:40 -0400

    Update issue templates

---
+++ TradeExpertFx/src/main/java/org/tradeexpert/tradeexpertfx/HelloResource.java
@@ -0,0 +1,14 @@
+package org.tradeexpert.tradeexpertfx;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
