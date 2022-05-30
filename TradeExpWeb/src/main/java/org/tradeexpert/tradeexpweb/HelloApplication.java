acf8051016a137551e56e19895b1865d4ebe55eb
Noel M Nguemechieu <48719845+nguemechieu@users.noreply.github.com>
2022-05-29 20:33:45 -0400

    news design

--- TradeExpWeb/src/main/java/org/tradeexpert/tradeexpweb/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package org.tradeexpert.tradeexpweb;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
---
+++ TradeExpert/src/main/java/org/tradeexperts/tradeexpert/TradeExpertApp.java
@@ -0,0 +1,10 @@
+package org.tradeexperts.tradeexpert;
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class TradeExpertApp extends Application {
+
+}
